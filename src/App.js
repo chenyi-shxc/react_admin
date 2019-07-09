061b3ce0ccf452f8d8bacc3e3cbf13db13024bcf
chenyi <61664653@qq.com>
2019-07-10 05:21:16 +0800

    引入antd，配置antd的按需加载

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React, { Component } from "react";
-
+import { Button, message } from "antd";
 export default class App extends Component {
+  handleClick = () => {
+    message.success("you click!");
+  };
   render() {
-    return <div>hello world</div>;
+    return (
+      <Button type="primary" onClick={this.handleClick}>
+        primary button
+      </Button>
+    );
   }
 }
